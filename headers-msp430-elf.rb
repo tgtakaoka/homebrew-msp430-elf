55b996a6c947bbcdd4ee8d87e21c57dfbc2a7d85
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-01-18 20:45:55 +0900

    Update to MSP430-GCC-OPENSOURCE_8.3.1.0

--- headers-msp430-elf.rb
+++ headers-msp430-elf.rb
@@ -1,6 +1,6 @@
 class HeadersMsp430Elf < Formula
   homepage "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/index_FDS.html"
-  url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_0_0/exports/msp430-gcc-support-files-1.208.zip"
+  url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_1_0/export/msp430-gcc-support-files-1.208.zip"
   sha256 "d9fc05de5ff4903ad0784446d1517ba81017c3f4ec3cfa0bab2cf9416084f1fa"
   revision 1
 
