9d42ffa5c85e8029c0c3f88f1f6e30a66ddfa5f1
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-07-11 20:58:31 +0900

    MSP430-GCC-OPENSOURCE_9_2_0_0

--- gcc-msp430-elf.rb
+++ gcc-msp430-elf.rb
@@ -1,9 +1,9 @@
 class GccMsp430Elf < Formula
   homepage "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/index_FDS.html"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz"
-  sha256 "64baadfe6cc0f4947a84cb12d7f0dfaf45bb58b7e92461639596c21e02d97d2c"
-  version "8.3.2-xx"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz"
+  sha256 "ea6ef08f121239da5695f76c9b33637a118dcf63e24164422231917fa61fb206"
+  version "9.2.0-50"
   revision 1
 
   depends_on "binutils-msp430-elf"
@@ -14,9 +14,9 @@ class GccMsp430Elf < Formula
   depends_on "isl" => :build if OS.mac?
 
   patch :p0 do
-    url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_2_2/export/msp430-gcc-8.3.1.25-source-patches.tar.bz2"
-    sha256 "a100f09f45c27f2c272718c5f3a90c00e3039fa6560ccc054ad5449d470054fa"
-    apply "gcc-8_3_0-release.patch"
+    url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/9_2_0_0/export/msp430-gcc-9.2.0.50-source-patches.tar.bz2"
+    sha256 "0756421396b5806bf3ac07d78a80ab999b82ead71cb3370a601c628f7bf1d3d3"
+    apply "gcc-9_2_0-release.patch"
   end
 
   resource "newlib" do
@@ -24,8 +24,8 @@ class GccMsp430Elf < Formula
     sha256 "545b3d235e350d2c61491df8b9f775b1b972f191380db8f52ec0b1c829c52706"
 
     patch :p0 do
-      url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_2_2/export/msp430-gcc-8.3.1.25-source-patches.tar.bz2"
-      sha256 "a100f09f45c27f2c272718c5f3a90c00e3039fa6560ccc054ad5449d470054fa"
+      url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/9_2_0_0/export/msp430-gcc-9.2.0.50-source-patches.tar.bz2"
+      sha256 "0756421396b5806bf3ac07d78a80ab999b82ead71cb3370a601c628f7bf1d3d3"
       apply "newlib-2_4_0.patch"
     end
   end
--- headers-msp430-elf.rb
+++ headers-msp430-elf.rb
@@ -1,7 +1,7 @@
 class HeadersMsp430Elf < Formula
   homepage "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/index_FDS.html"
-  url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_2_2/export/msp430-gcc-support-files-1.209.zip"
-  sha256 "30ca61e52edd025ec755ab8665c887782613a228ad0875bb24dbe38d68a435a4"
+  url "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/9_2_0_0/export/msp430-gcc-support-files-1.210.zip"
+  sha256 "eeedbfc7b1775e06e44c65d38977f998f6a9ee46b1a4cfcc194181c44bbd8911"
   revision 1
 
   def install
